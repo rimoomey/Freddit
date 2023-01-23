01a51fd4dc5bdfbee8ad75ade619272d6ec4bcc7
Scott Meadows <Scottmeadows04@gmail.com>
2023-01-23 12:47:30 -0500

    add browser routing

---
+++ client/src/components/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <h3 style={{color: 'red'}}>Oops! This page does not exist (404)</h3>
+  );
+}
--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useParams} from 'react-router-dom';
 
 export default function Post() {
+  const { topic_name: topicName, post_id: postId } = useParams();
+
   return (
-    <div>Post</div>
+    <>
+      <div>Topic</div>
+      <h3>This post is about: {topicName}</h3>
+      <h5>Post id is: {postId}</h5>
+    </>
   );
 }
