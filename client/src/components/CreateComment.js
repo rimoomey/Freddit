e8dc451b975bbf740895c7b30849b486ba1fcbab
Scott Meadows <Scottmeadows04@gmail.com>
2023-01-24 15:53:35 -0500

    add controlled form for create comment

--- client/src/components/CreateComment.js
+++ client/src/components/CreateComment.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import { Button } from '../styled-components/Button';
@@ -13,17 +13,27 @@ const MAX_ROWS = 10;
 const LINE_HEIGHT = 24;
 
 export default function CreateComment() {
+  const [text, setText] = useState('');
+
   const handleChange = e => {
     e.target.rows = MIN_ROWS;
     const numRows = Math.max((e.target.scrollHeight / LINE_HEIGHT), MIN_ROWS);
     e.target.rows = numRows < MAX_ROWS ? numRows : MAX_ROWS;
+    setText(e.target.value);
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(text);
+    setText('');
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <TextArea
         cols="100"
         rows={MIN_ROWS}
+        value={text}
         onChange={handleChange}
       />
       <PostButton as='input' type='submit' value='Post' />
