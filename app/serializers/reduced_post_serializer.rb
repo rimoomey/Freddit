7bcddb0b17a380b3a6e208555bca26b7f1e33ae2
Rimon Moomey <rimon.moomey@gmail.com>
2023-01-25 13:34:29 -0600

    post show includes user information

--- app/serializers/reduced_post_serializer.rb
+++ app/serializers/reduced_post_serializer.rb
@@ -2,6 +2,8 @@ class ReducedPostSerializer < ActiveModel::Serializer
   attributes :id, :title, :topic_name
 
   def topic_name
+    return nil if object.topic.nil?
+    
     object.topic.name
   end
 end
