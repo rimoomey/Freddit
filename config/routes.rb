2fad8cb28254c3f812e4517edb2aa51a9ed8b7bf
Rimon Moomey <rimon.moomey@gmail.com>
2023-01-25 15:19:18 -0600

    remove unnecessary rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,4 @@ Rails.application.routes.draw do
 
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
