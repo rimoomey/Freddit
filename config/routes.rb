93d6a8801c6fb4c65dc2c31b11f6a17d619f12fb
Rimon Moomey <rimon.moomey@gmail.com>
2023-01-26 14:42:44 -0600

    create build settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show, :create]
-  resources :users do
-    resources :posts, only: [:index, :create, :destroy, :update]
-    resources :comments, only: [:index, :create]
-    resources :likes, only: [:index, :create]
-  end
-  resources :posts, only: [:index, :show]
-  resources :posts do
-    resources :comments, only: [:index, :create]
-  end
-  resources :topics, only: [:index, :show]
-  resources :comments, only: [:index]
+  namespace :api do
+    resources :users, only: [:index, :show, :create]
+    resources :users do
+      resources :posts, only: [:index, :create, :destroy, :update]
+      resources :comments, only: [:index, :create]
+      resources :likes, only: [:index, :create]
+    end
+    resources :posts, only: [:index, :show]
+    resources :posts do
+      resources :comments, only: [:index, :create]
+    end
+    resources :topics, only: [:index, :show]
+    resources :comments, only: [:index]
 
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  get '/me', to: 'sessions#show'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+    get '/me', to: 'sessions#show'
+  end
+
+  get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
 end
