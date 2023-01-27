e089d9513f8f4481cee0200648a4608ee3dd01e5
Ben Merryman <ben.merryman@gmail.com>
2023-01-27 11:25:00 -0500

    footer in progress

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,14 +6,9 @@ import logo from '../assets/fr-logo.png'
 
 import SignupButton from './SignupButton'
 
-const TitleContainer = styled.div`
+const TitleContainer = styled(Link)`
   display: flex;
-  align-items: center;
-`
-
-const IconLink = styled(Link)`
-  display: flex;
-  align-items: start;
+  align-items: end;
 `
 
 const Icon = styled.img`
@@ -22,23 +17,7 @@ const Icon = styled.img`
   max-width: 5rem;
   margin: 0.5em 1em;
 `
-// Need to have title outside link, only icon will be linked
-const Title = styled.h1`
-  color: black;
-  text-decoration: none;
-  @media screen and (max-width: 720px){
-    display: none;
-  }
-`
-const SubTitle = styled.h2`
-  color: black;
-  text-decoration: none;
-  @media screen and (max-width: 1000px){
-    display: none;
-  }
-`
-
-const Head = styled.header`
+const Footer = styled.footer`
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -50,25 +29,14 @@ const SignupDiv = styled.div`
   padding: 0 1em;
 `
 
-const SignupBlurb = styled.p`
-  @media screen and (max-width: 720px){
-    display: none;
-  }
-`
-
-export default function Header () {
+export default function Footer () {
   const user = useSelector(state => state.user) // user stores user state data
 
   return (
-    <Head>
-      <TitleContainer>
-        <IconLink to='/'>
-          <Icon src={logo} alt='icon' />
-        </IconLink>
-
-        <Title>{`  Freddit  `}</Title>
-        <hr />
-        <SubTitle>⚠️ This is NOT Reddit ⚠️</SubTitle>
+    <Footer>
+      <TitleContainer to='/'>
+        <Icon src={logo} alt='icon' />
+        {/* <Title>Freddit</Title> */}
       </TitleContainer>
 
       {user.id ? (
@@ -76,12 +44,12 @@ export default function Header () {
       ) : (
         <SignupDiv>
           {/* Signup OR Signout */}
-          <SignupBlurb>
+          <p>
             Want to join all the REAL front pagers at Freddit? Sign up today!
-          </SignupBlurb>
+          </p>
           <SignupButton />
         </SignupDiv>
       )}
-    </Head>
+    </Fo>
   )
 }
